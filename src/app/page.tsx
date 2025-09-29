4a088e006373eeef3259f9cb87415d78ccdfe89b
usamimi39 <usamin04tp@gmail.com>
2025-09-29 18:05:08 +0900

    feat: ボタンコンポーネントの作成

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,12 @@
-"use client";
-
+import { HandSelectButton } from "./components/HnadSelectButton";
 
 export default function Home() {
   return (
-    <main className="flex items-center justify-center min-h-screen gap-10">
-      <button className="size-48 rounded-full border border-cyan-400 background gb-cyan-400">
-      </button>
-      <button className="size-48 rounded-full border border-red-400 background gb-red-400">
-      </button>
-      <button className="size-48 rounded-full border border-yellow-400 background gb-yellow-400">
-      </button>
-    </main>
+    <div className="flex items-center justify-center min-h-screen gap-4">
+      <HandSelectButton hand="rock" />
+      <HandSelectButton hand="scissors" />
+      <HandSelectButton hand="paper" />
+    </div>
   );
 }
 
